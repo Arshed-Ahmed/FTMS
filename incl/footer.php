13a27071f6164e41af6c9bf25fb7f3b003602415
Arshed <arshedahmed98@gmail.com>
2022-03-24 10:37:23 +0530

    Update footer.php

--- incl/footer.php
+++ incl/footer.php
@@ -1,15 +1,16 @@
           <!-- footer content -->
           <footer>
             <div class="pull-right">
-              Copyright © 2018 Colorlib. All rights reserved. Template by <a href="https://colorlib.com">Colorlib</a>.
+              Copyright © <?php echo (date("Y")); ?> Arshed. All rights reserved. Template by <a href="https://colorlib.com">Colorlib</a>.
             </div>
             <div class="clearfix"></div>
           </footer>
           <!-- /footer content -->
-        </div>
-        <!-- /main_container -->
-      </div>
-      <!-- /container body -->
-      <?php require_once('scripts.php');?>
-  </body>
-</html>
+          </div>
+          <!-- /main_container -->
+          </div>
+          <!-- /container body -->
+          <?php require_once('scripts.php'); ?>
+          </body>
+
+          </html>
