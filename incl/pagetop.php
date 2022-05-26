1ef7eb43513917f09a283c7f4c16b6c00efa8957
Arshed <arshedahmed98@gmail.com>
2022-05-27 00:39:31 +0530

    Update pagetop.php

--- incl/pagetop.php
+++ incl/pagetop.php
@@ -1,20 +1,20 @@
  <!-- BREADCRUMB-->
-<div class="right_col" role="main">
-  <div class="">
-    <div class="page-title">
-      <div class="title_left">
-        <h3 id="breadcrumb"></h3>
-      </div>
+ <div class="right_col" role="main">
+   <div class="">
+     <div class="page-title">
+       <div class="title_left">
+         <h3 id="breadcrumb"></h3>
+       </div>
 
-      <div class="title_right">
-        <div class="col-md-5 col-sm-5 col-xs-12 form-group pull-right top_search">
-          <div class="input-group">
-            <input type="text" class="form-control" placeholder="Search for...">
-            <span class="input-group-btn">
-              <button class="btn btn-default" type="button">Go!</button>
-            </span>
-          </div>
-        </div>
-      </div>
-    </div>
-<!--END BREADCRUMB  -->
+       <div class="title_right">
+         <div class="col-md-5 col-sm-5 col-xs-12 form-group pull-right top_search">
+           <div class="input-group">
+             <input type="text" class="form-control" placeholder="Search for...">
+             <span class="input-group-btn">
+               <button class="btn btn-default" type="button">Go!</button>
+             </span>
+           </div>
+         </div>
+       </div>
+     </div>
+     <!--END BREADCRUMB  -->
